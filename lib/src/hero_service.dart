30fee5c5a6900a09f21dada183c13577003a9d28
Ezra Moore <Ezra.Moore@guard.com>
2020-02-07 15:40:01 -0500

    Setups for Select a dashboard hero

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -4,4 +4,7 @@ import 'mock_heros.dart';
 
 class HeroService {
   Future<List<Hero>> getAll() async => mockHeroes;
+
+  Future<Hero> get(int id) async =>
+    (await getAll()).firstWhere((hero) => hero.id == id);
 }
